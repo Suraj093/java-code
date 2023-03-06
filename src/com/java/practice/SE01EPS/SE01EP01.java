06a99c4f1dea504c9a3907b699f04ef3b25ed40d
Suraj Kushwaha <Kushwahasuraj093@gmail.com>
2023-03-06 23:32:23 +0530

    Add code in the class

--- src/com/java/practice/SE01EPS/SE01EP01.java
+++ src/com/java/practice/SE01EPS/SE01EP01.java
@@ -1,10 +1,22 @@
 package com.java.practice.SE01EPS;
 
+import java.util.function.Predicate;
+
 public class SE01EP01 {
 	
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to programming world..");
+		
+		System.out.println("***********************************");
+		
+		Predicate<Integer> p= m -> m > 20;
+		
+		if(p.test(30)) {
+			System.out.println("Number is grater than the given number");
+		}else {
+			System.out.println("Number is not grater than the given number");
+		}
 		
 	}
 
