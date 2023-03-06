2a58abf558a964cc9f80c0f589abe16cd7c6de51
Suraj Kushwaha <Kushwahasuraj093@gmail.com>
2023-03-06 23:22:33 +0530

    first commit

---
+++ src/com/java/practice/SE01EPS/SE01EP01.java
@@ -0,0 +1,11 @@
+package com.java.practice.SE01EPS;
+
+public class SE01EP01 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to programming world..");
+		
+	}
+
+}
